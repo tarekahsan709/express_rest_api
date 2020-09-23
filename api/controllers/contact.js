615e17ee296783ef8b54bf7a54b9f25cea56d4a1
md.atikur rahman <atik0ahasan@gmail.com>
2020-09-23 23:54:03 +0600

    initial commit

---
+++ api/controllers/contact.js
@@ -0,0 +1,238 @@
+const Contact = require('../models/Contact')
+const getAllContactController = (req, res, next) => {
+
+    Contact.find()
+        .then(contacts => {
+            res.status(200).json({
+
+                message: 'All contacts',
+                contacts
+
+            })
+
+
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+
+                message: `error occured`,
+                error: err
+
+
+            })
+
+        })
+
+
+
+
+}
+
+
+
+const postNewContactController = (req, res, next) => {
+
+    contact = new Contact({
+
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email
+
+
+    })
+
+    contact.save()
+        .then(data => {
+            res.status(201).json({
+                message: 'Contact Added',
+                contact: data
+
+            })
+
+        })
+        .catch(err => {
+
+            console.log(err)
+            res.status(500).json({
+                message: 'error occured',
+                error: err
+
+            })
+
+
+        })
+
+
+}
+
+
+const getSingleContact = (req,res,next) => {
+
+
+    let id = req.params.id
+   Contact.findById(id)
+
+      .then(contact => {
+            res.status(200).json({
+
+                  contact
+
+
+            })
+
+      })
+       .catch(err => {
+
+           console.log(err)
+           res.status(500).json({
+               message: 'error occured',
+               error: err
+
+           })
+
+
+       })
+
+
+
+}
+
+
+
+
+
+const deleteContact =(req,res,next) => {
+
+    let id = req.params.id
+    Contact.findByIdAndRemove(id)
+    .then( result => {
+
+         res.json({
+
+            message: 'Contact deleted',
+            result
+
+
+         })
+    })
+        .catch(err => {
+
+            console.log(err)
+            res.status(500).json({
+                message: 'error occured',
+                error: err
+
+            })
+
+
+        }) 
+
+
+}
+
+
+
+const editrContact = (req, res, next) => {
+
+    let id = req.params.id
+    let updatedContact ={
+
+           name: req.body.name,
+           phone: req.body.name,
+           email: req.body.email
+
+    }
+     
+   Contact.findByIdAndUpdate(id,{$set: updatedContact })
+
+
+
+
+             .then(contact => {
+
+
+
+
+                Contact.findById(contact._id)
+                .then(newContact => {
+
+                    res.json({
+
+
+                        message: `Updated Successfully`,
+                        contact
+
+
+                    })
+
+
+
+                })
+
+
+
+
+            
+
+
+
+             })
+             .catch(err => {
+
+           console.log(err)
+           res.status(500).json({
+               message: 'error occured',
+               error: err
+
+           })
+
+
+       })
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    
+
+
+    postNewContactController,
+    getAllContactController,
+    getSingleContact,
+    deleteContact,
+    editrContact
+
+
+}
---
+++ api/models/Contact.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+const Schema = mongoose.Schema
+
+const contactSchema = new Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3
+
+    },
+    phone: {
+        type: String,
+         trim: true,
+
+        require: true,
+        unique: true
+
+
+
+
+    },
+    email: {
+        type: String,
+        trim: true,
+
+        require: true,
+        validate: {
+           validator: (v) => {
+                  
+            return validator.isEmail(v)
+
+           },
+           message: `{VALUE} is not an email`
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+)
+
+
+
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+module.exports = Contact
