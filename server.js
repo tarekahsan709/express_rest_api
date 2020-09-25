26c8909adcfb46d02b0ea06742f2c2e407259bd5
Muhammad.Nazmul Ahsan <tarekahsan709@gmail.com>
2020-09-25 23:36:46 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,84 +11,15 @@ const db = mongoose.connection
 
 db.on('error', (err) => {
     console.error('Database error:',err);
-}
-
-)
+})
 
 db.once('open', () => {
     console.log('Database connected Successfully')
-}
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 const contactRoute = require('./api/routes/contact')
 
-/*
-const schema = mongoose.Schema
-const contactSchema= new Schema({
-    
-         name: {
-            type: String,
-            required: true,
-            minlength: 3   
-
-         },
-         phone: {
-             type: String,
-             require: true  
-
-
-
-
-         }
-
-
-
-}
-
-
-
-)
-
-const Contact = mongoose.model('Contact',contactSchema)
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(express)
 const app = express()
-
-
 
 app.use(morgan('dev'))
 app.use(cors())
@@ -96,65 +27,23 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-
 const PORT = process.env.PORT || 3000
-
-
-/*
-app.use((req,res,next)=>{
-
-    console.log('i am middleware')
-    next()
-})
-
-*/
-
 
 app.use('/api/contacts', contactRoute)
 
 app.get('/', (req, res) => {
-
     res.send('<h1>Hello World</h1>')
-}
-
-)
+})
 
 app.get('/posts', (req, res) => {
-
     res.send('<h1>Another Route</h1>')
-}
-
-)
-
+})
 
 app.get('/api/contacts', (req, res) => {
-
     res.send('<h1>contacts</h1>')
-}
-
-)
-/*
-app.post('/api/contacts', (req, res) => {
-
-    res.json({
-
-        message: 'i am postman'
-    })
-}
-
-)
-
-*/
-
-
-
-
-
+})
 
 
 app.listen(PORT, () => {
-
     console.log(`server is running of PORT ${PORT}`)
-
 })
