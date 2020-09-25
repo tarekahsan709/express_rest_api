7ba2e044c90f8d85ab8aa339b69b9f72ad10e896
Muhammad.Nazmul Ahsan <tarekahsan709@gmail.com>
2020-09-25 23:38:42 +0600

    Update contact.js

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,58 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-
- const Contact = require('../models/Contact')
+const Contact = require('../models/Contact')
 
 const contactController = require('../controllers/contact')
 
 
-//get
-
 router.get('/', contactController.getAllContactController)
-
-
-
-
-/*    router.get('/:id', (req, res, next) => {
-
-     const id= req.params.id
-   
-    res.json({
-
-        id
-
-    })
-})
-
-*/
-
-router.post('/', contactController.postNewContactController )
-
-
-
-
 router.get('/:id',contactController.getSingleContact)
-
-
-
-
-
-
-
-
+router.post('/', contactController.postNewContactController )
 router.put('/:id',contactController.editrContact)
-
-
-
 router.delete('/:id',contactController.deleteContact)
-
-
-
-
-
-
-
-
 
 module.exports = router 
